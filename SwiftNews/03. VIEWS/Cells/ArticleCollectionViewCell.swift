ea4dfeef847e91bfe430a4c565fbdc8f834cbef1
Bartolome Estelrich <b.estelrich@mycloudportion.com>
2020-02-12 16:34:38 -0500

    CollectionView
    ArticlesCollectionView its respective cell have been configured. Interface now navigates through views as expected.

---
+++ SwiftNews/03. VIEWS/Cells/ArticleCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ArticleCollectionViewCell.swift
+//  SwiftNews
+//
+//  Created by BES on 2020-02-12.
+//  Copyright © 2020 BEstelrich. All rights reserved.
+//
+
+import UIKit
+
+//@IBDesignable
+class ArticleCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var articleTitleLabel: UILabel!
+  @IBOutlet weak var articleImageView: UIImageView!
+  
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    configure()
+  }
+  
+//  override func prepareForInterfaceBuilder() {
+//    super.prepareForInterfaceBuilder()
+//    configure()
+//  }
+  
+  
+  private func configure() {
+    self.layer.cornerRadius  = 25
+  }
+  
+}
