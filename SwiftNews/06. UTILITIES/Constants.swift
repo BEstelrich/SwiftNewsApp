ae24255c5372eb550c47342d97267c6d4850f22b
Bartolome Estelrich <b.estelrich@mycloudportion.com>
2020-02-12 16:29:45 -0500

    Constants
    New class named Constants created to store them being accessible from everywhere within the app.

---
+++ SwiftNews/06. UTILITIES/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  SwiftNews
+//
+//  Created by BES on 2020-02-12.
+//  Copyright © 2020 BEstelrich. All rights reserved.
+//
+
+import Foundation
+
+enum Identifier {
+  
+  enum Segue {
+    static let fromArticlesToArticleDetails: String = "FromArticlesToArticleDetails"
+  }
+  
+  
+  enum Cell {
+    static let articleCollectionViewCell: String    = "ArticleCollectionViewCell"
+  }
+  
+}
